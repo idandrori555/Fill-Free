ce2247f07e6c38fd785fa5094def8d79f14c8d7c
idan <idandrori555@gmail.com>
2025-11-02 11:10:46 +0200

    removed all unnecessary html files

--- App.tsx
+++ App.tsx
@@ -3,27 +3,27 @@ import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.safeArea}>
-            <StatusBar
-                barStyle={Platform.OS === "ios" ? "dark-content" : "default"}
-                backgroundColor="transparent"
-                translucent={Platform.OS === "android" ? true : false}
-            />
-            <WebView
-                originWhitelist={["*"]}
-                source={require("./assets/high_score.html")}
-                style={styles.webView}
-            />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar
+        barStyle={Platform.OS === "ios" ? "dark-content" : "default"}
+        backgroundColor="transparent"
+        translucent={Platform.OS === "android" ? true : false}
+      />
+      <WebView
+        originWhitelist={["*"]}
+        source={require("./assets/index.html")}
+        style={styles.webView}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-    },
-    webView: {
-        flex: 1,
-    },
+  safeArea: {
+    flex: 1,
+  },
+  webView: {
+    flex: 1,
+  },
 });
